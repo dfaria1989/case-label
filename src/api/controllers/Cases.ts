e679668aaf0b3c1970737c947b5adf98e7d90349
Daniel Faria <daniel.faria@pamafe.pt>
2021-12-07 01:55:37 +0000

    add query param to get cases by viewed=false

--- src/api/controllers/Cases.ts
+++ src/api/controllers/Cases.ts
@@ -4,7 +4,8 @@ import CaseService from '../../services/case';
 export default class Cases {
   static async findAll(req: Request, res: Response): Promise<any> {
     try {
-      const response = await CaseService.findAll();
+      const { viewed }: { viewed?: boolean } = req.query;
+      const response = await CaseService.findAll(viewed);
       return res.status(200).json(response);
     } catch (error: any) {
       return res.status(500).json(error.message);
--- src/services/case.ts
+++ src/services/case.ts
@@ -1,9 +1,10 @@
 import CaseModel from '../models/case';
 
 export default class Case {
-  static async findAll() {
+  static async findAll(viewed: boolean | undefined) {
+    const filter = (viewed && { viewed: viewed }) || {};
     try {
-      const responseData = await CaseModel.find();
+      const responseData = await CaseModel.find(filter);
       return { status: true, message: responseData };
     } catch (error: any) {
       throw new Error(error);
