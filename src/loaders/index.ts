e915f21e2a5196c81eb460d0cc83eb95d5e5f254
Daniel Faria <daniel.faria@pamafe.pt>
2021-12-20 11:05:46 +0000

    minor conf changes

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,7 +2,6 @@ import expressLoader from './express';
 import LoggerInstance from './winston';
 import './mongo';
 
-console.log('aqqui');
 const expressInit = ({ expressApp }: { expressApp: any }): any => expressLoader({ app: expressApp });
 const Logger = LoggerInstance;
 export { expressInit, Logger };
